57df4686796b57345a8ccf1a2676467cfad03235
Adam Black <adamblack@microsoft.com>
2022-03-29 14:54:56 +0100

    Add project files.

---
+++ AvivaWebApp3.5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AvivaWebApp3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
